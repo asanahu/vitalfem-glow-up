1c5af34e3ea25e5d9a3cd43a1f7c7005a2981f2d
root <root@srv1318715.hstgr.cloud>
2026-09-11 19:40:16 +0000

    Add canonical URLs to every page

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import { useLocation } from "react-router-dom";
 
 interface SEOProps {
     title: string;
@@ -10,26 +11,29 @@ interface SEOProps {
 const SEO = ({
     title,
     description,
-    url = "https://www.vitalfem.es",
+    url,
     image = "/og-image.png"
 }: SEOProps) => {
+    const location = useLocation();
+    const canonicalUrl = url ?? `https://www.vitalfem.es${location.pathname === "/" ? "" : location.pathname}`;
     const fullTitle = `${title} | VitalFem`;
 
     return (
         <Helmet>
             <title>{fullTitle}</title>
             <meta name="description" content={description} />
+            <link rel="canonical" href={canonicalUrl} />
 
             {/* Open Graph / Facebook */}
             <meta property="og:type" content="website" />
-            <meta property="og:url" content={url} />
+            <meta property="og:url" content={canonicalUrl} />
             <meta property="og:title" content={fullTitle} />
             <meta property="og:description" content={description} />
             <meta property="og:image" content={image} />
 
             {/* Twitter */}
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:url" content={url} />
+            <meta name="twitter:url" content={canonicalUrl} />
             <meta name="twitter:title" content={fullTitle} />
             <meta name="twitter:description" content={description} />
             <meta name="twitter:image" content={image} />
